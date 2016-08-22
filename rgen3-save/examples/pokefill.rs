b0768f2ad2185e0450dff16e7badf9be837e824b
Mika Attila <radiantstatue@gmail.com>
2016-08-22 17:46:01 +0200

    pokefill: Generate at least 1 pokemon for each species

--- rgen3-save/examples/pokefill.rs
+++ rgen3-save/examples/pokefill.rs
@@ -2,7 +2,7 @@ extern crate rgen3_save;
 extern crate rgen3_string;
 extern crate rand;
 
-use rand::{Rng, thread_rng};
+use rand::{Rng, thread_rng, ThreadRng};
 use rgen3_save::{Pokemon, SaveSections, TrainerInfo};
 use std::collections::HashSet;
 
@@ -18,83 +18,112 @@ static SUFFIX_NAMES: [&'static str; 23] = ["Adder", "Baker", "Biter", "Coder", "
                                            "Giver", "Laser", "Lover", "Maker", "Order", "Racer",
                                            "Taker", "Tamer", "Voter", "Waker", "Zoner"];
 
-fn gen_pokemon<R: Rng>(rng: &mut R,
-                       chosen_names: &mut HashSet<String>,
-                       trainer: &TrainerInfo)
-                       -> Pokemon {
-    let mut pokemon = Pokemon::default();
-    let mut name;
-    loop {
-        let prefix = rng.choose(&PREFIX_NAMES).unwrap();
-        let suffix = rng.choose(&SUFFIX_NAMES).unwrap();
-        name = format!("{} {}", prefix, suffix);
-        // Make sure we don't use the same name twice in our team
-        if !chosen_names.contains(&name) {
-            break;
+struct PokeGen<'a> {
+    chosen_names: HashSet<String>,
+    chosen_species: HashSet<u16>,
+    n_unique: u16,
+    trainer: &'a TrainerInfo,
+    rng: ThreadRng,
+}
+
+impl<'a> PokeGen<'a> {
+    fn new(trainer: &'a TrainerInfo) -> Self {
+        PokeGen {
+            chosen_names: HashSet::new(),
+            chosen_species: HashSet::new(),
+            n_unique: 0,
+            trainer: trainer,
+            rng: thread_rng(),
         }
     }
-    rgen3_string::encode_string(&name, &mut pokemon.nickname.0);
-    chosen_names.insert(name);
-    {
-        if pokemon.active_data.is_none() {
-            pokemon.active_data = Some(Default::default());
+    fn gen(&mut self) -> Pokemon {
+        let mut pokemon = Pokemon::default();
+        let mut name;
+        loop {
+            let prefix = self.rng.choose(&PREFIX_NAMES).unwrap();
+            let suffix = self.rng.choose(&SUFFIX_NAMES).unwrap();
+            name = format!("{} {}", prefix, suffix);
+            // Make sure we don't use the same name twice in our team
+            if !self.chosen_names.contains(&name) {
+                break;
+            }
         }
+        rgen3_string::encode_string(&name, &mut pokemon.nickname.0);
+        self.chosen_names.insert(name);
         {
-            let active_data = pokemon.active_data.as_mut().unwrap();
-            pokemon.personality = rng.gen();
-            active_data.level = rng.gen_range(80, 100);
-            active_data.total_hp = rng.gen_range(800, 999);
-            active_data.current_hp = active_data.total_hp;
-            active_data.attack = rng.gen_range(800, 999);
-            active_data.defense = rng.gen_range(800, 999);
-            active_data.speed = rng.gen_range(800, 999);
-            active_data.sp_attack = rng.gen_range(800, 999);
-            active_data.sp_defense = rng.gen_range(800, 999);
+            if pokemon.active_data.is_none() {
+                pokemon.active_data = Some(Default::default());
+            }
+            {
+                let active_data = pokemon.active_data.as_mut().unwrap();
+                pokemon.personality = self.rng.gen();
+                active_data.level = self.rng.gen_range(80, 100);
+                active_data.total_hp = self.rng.gen_range(800, 999);
+                active_data.current_hp = active_data.total_hp;
+                active_data.attack = self.rng.gen_range(800, 999);
+                active_data.defense = self.rng.gen_range(800, 999);
+                active_data.speed = self.rng.gen_range(800, 999);
+                active_data.sp_attack = self.rng.gen_range(800, 999);
+                active_data.sp_defense = self.rng.gen_range(800, 999);
+            }
+            loop {
+                let species = self.rng.gen_range(1, 412);
+                let result = pokemon.set_species(species);
+                if result.is_ok() {
+                    if self.chosen_species.contains(&species) {
+                        if self.n_unique >= 386 {
+                            break;
+                        }
+                    } else {
+                        assert!(self.chosen_species.insert(species));
+                        self.n_unique += 1;
+                        break;
+                    }
+                }
+            }
+            pokemon.data.growth.experience = 1_640_000;
+            pokemon.data.growth.friendship = 0xFF;
+            pokemon.data.growth.pp_bonuses = 0xFF;
+            pokemon.data.attacks.move1 = self.rng.gen_range(0, 354);
+            pokemon.data.attacks.move2 = self.rng.gen_range(0, 354);
+            pokemon.data.attacks.move3 = self.rng.gen_range(0, 354);
+            pokemon.data.attacks.move4 = self.rng.gen_range(0, 354);
+            pokemon.data.attacks.pp1 = 99;
+            pokemon.data.attacks.pp2 = 99;
+            pokemon.data.attacks.pp3 = 99;
+            pokemon.data.attacks.pp4 = 99;
+            pokemon.data.evs_and_condition.hp = 0xFF;
+            pokemon.data.evs_and_condition.attack = 0xFF;
+            pokemon.data.evs_and_condition.defense = 0xFF;
+            pokemon.data.evs_and_condition.speed = 0xFF;
+            pokemon.data.evs_and_condition.sp_attack = 0xFF;
+            pokemon.data.evs_and_condition.sp_defense = 0xFF;
+            pokemon.data.evs_and_condition.coolness = 0xFF;
+            pokemon.data.evs_and_condition.beauty = 0xFF;
+            pokemon.data.evs_and_condition.cuteness = 0xFF;
+            pokemon.data.evs_and_condition.smartness = 0xFF;
+            pokemon.data.evs_and_condition.toughness = 0xFF;
+            pokemon.ot_id = self.trainer.full_id();
+            pokemon.ot_name = self.trainer.name;
         }
-        while pokemon.set_species(rng.gen_range(1, 412)).is_err() {}
-        pokemon.data.growth.experience = 1_640_000;
-        pokemon.data.growth.friendship = 0xFF;
-        pokemon.data.growth.pp_bonuses = 0xFF;
-        pokemon.data.attacks.move1 = rng.gen_range(0, 354);
-        pokemon.data.attacks.move2 = rng.gen_range(0, 354);
-        pokemon.data.attacks.move3 = rng.gen_range(0, 354);
-        pokemon.data.attacks.move4 = rng.gen_range(0, 354);
-        pokemon.data.attacks.pp1 = 99;
-        pokemon.data.attacks.pp2 = 99;
-        pokemon.data.attacks.pp3 = 99;
-        pokemon.data.attacks.pp4 = 99;
-        pokemon.data.evs_and_condition.hp = 0xFF;
-        pokemon.data.evs_and_condition.attack = 0xFF;
-        pokemon.data.evs_and_condition.defense = 0xFF;
-        pokemon.data.evs_and_condition.speed = 0xFF;
-        pokemon.data.evs_and_condition.sp_attack = 0xFF;
-        pokemon.data.evs_and_condition.sp_defense = 0xFF;
-        pokemon.data.evs_and_condition.coolness = 0xFF;
-        pokemon.data.evs_and_condition.beauty = 0xFF;
-        pokemon.data.evs_and_condition.cuteness = 0xFF;
-        pokemon.data.evs_and_condition.smartness = 0xFF;
-        pokemon.data.evs_and_condition.toughness = 0xFF;
-        pokemon.ot_id = trainer.full_id();
-        pokemon.ot_name = trainer.name;
+        pokemon
     }
-    pokemon
 }
 
 fn main() {
     let mut args = std::env::args().skip(1);
     let path = args.next().expect("Need path to save as first arg");
     let mut save = rgen3_save::Save::load_from_file(&path).unwrap();
-    let mut rng = thread_rng();
     {
         let SaveSections { trainer, team, pc_boxes } = save.sections();
-        let mut chosen_names = HashSet::new();
+        let mut generator = PokeGen::new(trainer);
         team.clear();
         for _ in 0..6 {
-            team.push(gen_pokemon(&mut rng, &mut chosen_names, trainer));
+            team.push(generator.gen());
         }
         for b in pc_boxes.iter_mut() {
             for p in &mut b.pokemon {
-                *p = Some(gen_pokemon(&mut rng, &mut chosen_names, trainer));
+                *p = Some(generator.gen());
             }
         }
     }
